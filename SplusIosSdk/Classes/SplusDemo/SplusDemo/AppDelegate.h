85303e9119a402d2ce19165cc44b3ce2bfbad604
akzhang <ch@CHdeMac-mini.local>
2014-06-17 18:41:22 +0800

    sss
    sfvv

---
+++ SplusIosSdk/Classes/SplusDemo/SplusDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SplusDemo
+//
+//  Created by akzhang on 14-6-12.
+//  Copyright (c) 2014年 akzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SplusIosSdk/Classes/hero.h
@@ -0,0 +1,13 @@
+//
+//  hero.h
+//  SplusFramework
+//
+//  Created by akzhang on 14-6-12.
+//  Copyright (c) 2014年 akzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface hero : UIViewController
+
+@end
