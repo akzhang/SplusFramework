88c277af4c650bec8e18c8dae1d9d92260dcadc8
akzhang <ch@CHdeMac-mini.local>
2014-07-08 12:13:46 +0800

    不定额支付
    不定额支付

---
+++ SplusIosSdk/Classes/Payment/QutoPayHome.h
@@ -0,0 +1,57 @@
+//
+//  QutoPayHome.h
+//  SplusFramework
+//
+//  Created by akzhang on 14-7-3.
+//  Copyright (c) 2014年 akzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CollectionPayCell.h"
+#import "HttpUrl.h"
+#import "GetImage.h"
+#import "SplusCallback.h"
+#import "PSTCollectionView.h"
+#import "PayCreditCard.h"
+#import "PayOneCard.h"
+#import "PayManual.h"
+#import "MBProgressHUD.h"
+#import "CoinRatio.h"
+#import "AppInfo.h"
+#import "ActivateInfo.h"
+#import "MyMD5.h"
+#import "QutoPayWay.h"
+
+@interface QutoPayHome : UIViewController<PSTCollectionViewDelegate,PSTCollectionViewDataSource>
+
+
+//delegate
+@property(nonatomic, retain)id<SplusCallback> delegate;
+
+@property(nonatomic, strong)UIButton *back;
+
+@property(nonatomic, strong)UILabel *splusPayText;
+
+@property(nonatomic, strong)UIButton *custom;
+
+@property(nonatomic, strong)UILabel *splusWelText;
+
+//分割线
+@property(nonatomic, strong)UIImageView *splusSplitLine;
+
+//用户名
+@property(nonatomic, strong)UILabel *splusUserName;
+
+@property(nonatomic, strong)UILabel *splusChooseWay;
+
+//支付方式
+@property(nonatomic, strong)PSUICollectionView *splusPayWayCollection;
+
+//图片名字
+@property(nonatomic, strong)NSArray *imageNameArray;
+@property(nonatomic, strong)NSArray *imageNameArrayCh;
+
+//方向
+@property(nonatomic, assign)UIInterfaceOrientation orientation;
+
+@end
